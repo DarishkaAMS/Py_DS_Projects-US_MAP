36f321a4ac9b7ba45b91b4961680ff64be9dcd25
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-10-22 08:21:29 +0300

    Add circle marker:

--- map.py
+++ map.py
@@ -20,7 +20,17 @@ folium.Marker([50.4698435, 30.6487035], popup='<strong>Location Two</strong>',
               tooltip=tooltip, icon=folium.Icon(icon='cloud', color='purple')).add_to(m),
 
 folium.Marker([50.4398435, 30.6287035], popup='<strong>Location AMS</strong>',
-              tooltip=tooltip, icon=logoIcon).add_to(m)
+              tooltip=tooltip, icon=logoIcon).add_to(m),
+
+# Circle marker
+folium.CircleMarker(
+    location=[50.432043, 30.625697],
+    radius=50,
+    popup='Having Fun',
+    color='#428bca',
+    fill=True,
+    fill_color='#428bca'
+).add_to(m)
 
 # Generate map in html
 m.save('map.html')
