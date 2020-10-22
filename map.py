b9a326d7d4e38a21fa51e35febeccd17b203c77e
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-10-22 08:15:17 +0300

    Add custom icon marker

--- map.py
+++ map.py
@@ -6,15 +6,21 @@ m = folium.Map(location=[50.4343, 30.5277], zoom_start=12)
 # Global tooltip
 tooltip = 'Click for more info'
 
+# Create custom marker icon
+logoIcon = folium.features.CustomIcon('d_logo.png', icon_size=(50, 50))
+
 # Create markers
 folium.Marker([50.4494865, 30.6586025], popup='<strong>Location One</strong>',
-              tooltip=tooltip).add_to(m)
+              tooltip=tooltip).add_to(m),
 
 folium.Marker([50.4598435, 30.6487035], popup='<strong>Location Two</strong>',
-              tooltip=tooltip, icon=folium.Icon(icon='cloud')).add_to(m)
+              tooltip=tooltip, icon=folium.Icon(icon='leaf')).add_to(m),
 
-folium.Marker([50.4598435, 30.6487035], popup='<strong>Location Two</strong>',
-              tooltip=tooltip, icon=folium.Icon(icon='cloud', color='purple')).add_to(m)
+folium.Marker([50.4698435, 30.6487035], popup='<strong>Location Two</strong>',
+              tooltip=tooltip, icon=folium.Icon(icon='cloud', color='purple')).add_to(m),
+
+folium.Marker([50.4398435, 30.6287035], popup='<strong>Location AMS</strong>',
+              tooltip=tooltip, icon=logoIcon).add_to(m)
 
 # Generate map in html
 m.save('map.html')
